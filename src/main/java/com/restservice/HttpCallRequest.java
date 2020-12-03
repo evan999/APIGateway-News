6a9f137a25d25b2884f0ab3f716da3e4379308d2
evan999 <emeshberge@gmail.com>
2020-12-02 23:09:26 -0500

    Work on API gatway.

---
+++ src/main/java/com/restservice/HttpCallRequest.java
@@ -0,0 +1,35 @@
+package com.restservice;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.http.HttpClient;
+
+public class HttpCallRequest {
+    
+    private static HttpURLConnection connection;
+    private final HttpClient httpClient = HttpClient.newBuilder()
+            .version(HttpClient.Version.HTTP_2)
+            .build();
+    
+    public static String httpCallRequest(String urlString) throws MalformedURLException{
+
+        try{
+            URL url = new URL(urlString);
+            connection = (HttpURLConnection) url.openConnection();
+
+            connection.setRequestMethod("GET");
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+
+            int status = connection.getResponseCode();
+
+            if(status > 200) {
+                reader = new BufferedReader(new InputStreamReader(connection.getErrorStream()))
+
+            }
+        }
+    }
+}
---
+++ src/main/java/com/restservice/UserController.java
@@ -0,0 +1,12 @@
+package com.restservice;
+
+import org.apache.catalina.User;
+
+import java.util.List;
+
+public class UserController {
+
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers()
+}
