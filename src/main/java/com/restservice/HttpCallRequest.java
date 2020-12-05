672370792f276edf2a5cc3db1a2761b5cc9acdb8
evan999 <emeshberge@gmail.com>
2020-12-04 23:42:14 -0500

    Some more work on API call request.

--- src/main/java/com/restservice/HttpCallRequest.java
+++ src/main/java/com/restservice/HttpCallRequest.java
@@ -10,11 +10,14 @@ import java.net.http.HttpClient;
 public class HttpCallRequest {
     
     private static HttpURLConnection connection;
+    /*
     private final HttpClient httpClient = HttpClient.newBuilder()
             .version(HttpClient.Version.HTTP_2)
             .build();
+     */
     
     public static String httpCallRequest(String urlString) throws MalformedURLException{
+        Buffered
 
         try{
             URL url = new URL(urlString);
@@ -25,6 +28,19 @@ public class HttpCallRequest {
             connection.setReadTimeout(5000);
 
             int status = connection.getResponseCode();
+
+            if(status > 200) {
+                reader
+            }
+
+            BufferedReader input = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream()));
+            )
+            String line;
+            while((line = reader.readLine()) != null) {
+                responseContent.append(line);
+            }
+            StringBuffer response = new StringBuffer();
         }
     }
 }
